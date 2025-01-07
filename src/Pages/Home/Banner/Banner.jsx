f4772a873c4508e2a9a52af50353324126f11a00
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-01-07 11:41:18 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import slide1 from "../../../assets/Slider/Slide-1.png";
+import slide2 from "../../../assets/Slider/Untitled design.png";
+import slide3 from "../../../assets/Slider/Slide-3.png";
+import "./Banner.css";
+
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <Carousel>
+      <div>
+        <img src={slide1} />
+      </div>
+      <div>
+        <img src={slide2} />
+      </div>
+      <div>
+        <img src={slide3} />
+      </div>
+    </Carousel>
+  );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,78 @@
+// import { Link } from 'react-router-dom';
+// import logo from '../../../assets/Logo/logo.svg'
+const Navbar = () => {
+  return (
+    <div className="navbar fixed z-10 bg-opacity-50 bg-black text-white ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+
+            <li>
+              <a>Packages</a>
+            </li>
+            <li>
+              <a>Activities</a>
+            </li>
+            <li>
+              <a>About Us</a>
+            </li>
+            <li>
+              <a>Contacts</a>
+            </li>
+          </ul>
+        </div>
+        {/* <Link to="/" className="btn btn-ghost normal-case text-xl">
+          <img className=" w-14 rounded-2xl mx-auto mt-0" src={logo} alt="logo" />
+        </Link> */}
+        {/* <a className="btn btn-ghost text-xl"><img src={logo} alt="" /></a> */}
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+
+          <li>
+            <a>Packages</a>
+          </li>
+          <li>
+            <a>Activities</a>
+          </li>
+          <li>
+            <a>About Us</a>
+          </li>
+          <li>
+            <a>Contacts</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
