c151effcf42924cebc3df9b850b5371b1c64f968
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-01-13 11:00:25 +0600

    v

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import slide1 from "../../../assets/Slider/Slide-1.png";
-import slide2 from "../../../assets/Slider/Slide-2.png";
-import slide3 from "../../../assets/Slider/Slide-3.png";
+import slide1 from "../../../assets/Slider/slide1.png";
+import slide2 from "../../../assets/Slider/slide2.png";
+import slide3 from "../../../assets/Slider/slide3.png";
 
 const Banner = () => {
   return (
