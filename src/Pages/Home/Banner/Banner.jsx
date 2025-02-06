d772df1e20a77a7f27abca11b7b0e97f49605d26
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-02-06 17:34:21 +0600

    U8

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,20 +3,73 @@ import { Carousel } from "react-responsive-carousel";
 import slide1 from "../../../assets/Slider/slide1.png";
 import slide2 from "../../../assets/Slider/slide2.png";
 import slide3 from "../../../assets/Slider/slide3.png";
+import "./Banner.css";
+import { useState } from "react";
 
 const Banner = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slides = [slide1, slide2, slide3];
   return (
-    <Carousel>
-      <div className="lg:h-[90vh]">
-        <img src={slide1} />
-      </div>
-      <div className="lg:h-[90vh]">
-        <img src={slide2} />
-      </div>
-      <div className="lg:h-[90vh]">
-        <img src={slide3} />
-      </div>
-    </Carousel>
+    // <Carousel
+    //   autoPlay
+    //   infiniteLoop
+    //   showThumbs={true}
+    //   showStatus={true}
+    //   showIndicators={false}
+    //   interval={3000}
+    //   transitionTime={800}
+    //   onChange={(index) => setCurrentSlide(index)}
+    // >
+    //   {/* <div className="carousel-item lg:h-[90vh]">
+    //   <img className="zoom-effect" src={slide1} />
+    // </div>
+    // <div className="carousel-item lg:h-[90vh]">
+    //   <img className="zoom-effect" src={slide2} />
+    // </div>
+    // <div className="carousel-item lg:h-[90vh]">
+    //   <img className="zoom-effect" src={slide3} />
+    // </div> */}
+    //   {slides.map((slide, index) => (
+    //     <div
+    //       key={index}
+    //       className={`carousel-item lg:h-[90vh] ${
+    //         index === currentSlide ? "zoom-effect" : ""
+    //       }`}
+    //     >
+    //       <img src={slide} />
+    //     </div>
+    //   ))}
+    // </Carousel>
+    <div className="h-full">
+      {" "}
+      {/* Ensures the parent has height */}
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showThumbs={true}
+        showStatus={false}
+        showIndicators={false}
+        interval={3000}
+        transitionTime={800}
+        onChange={(index) => setCurrentSlide(index)} // Update current slide index
+      >
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`carousel-item ${
+              index === currentSlide ? "zoom-effect" : ""
+            }`}
+          >
+            <img
+              src={slide}
+              className="w-full object-cover max-h-[90vh] transition-all duration-1000" // Apply max height for proper scaling
+              alt={`Slide ${index + 1}`}
+            />
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 };
 
