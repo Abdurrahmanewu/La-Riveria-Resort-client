5fa80937e4c9b1369082d7ae5fbfecd878851b88
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-04-23 13:05:12 +0600

    review del done

--- src/Pages/Home/Reviews/UserReview.jsx
+++ src/Pages/Home/Reviews/UserReview.jsx
@@ -4,11 +4,12 @@ import { AuthContext } from "../../../Context/AuthProvider/AuthProvider";
 import useAxios from "../../../Hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
 import UserReviewBox from "./UserReviewBox";
+import Swal from "sweetalert2";
 
 const UserReview = () => {
   const { user } = useContext(AuthContext);
   const axiosSecure = useAxios();
-  const { data: reviews = [] } = useQuery({
+  const { refetch, data: reviews = [] } = useQuery({
     queryKey: ["reviews", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/reviews?email=${user?.email}`);
@@ -16,6 +17,27 @@ const UserReview = () => {
     },
   });
   // console.log(reviews);
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/reviews/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            Swal.fire("Deleted!", "Your review has been deleted.", "success");
+            refetch();
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <DashBoardHeader
@@ -29,11 +51,15 @@ const UserReview = () => {
           </>
         }
       />
-      <h1>My reviews</h1>
-      {reviews.length}
-      {reviews.map((review) => {
-        console.log(review);
-      })}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {reviews.map((review) => (
+          <UserReviewBox
+            key={review._id}
+            review={review}
+            handleDelete={handleDelete}
+          ></UserReviewBox>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Reviews/UserReviewBox.jsx
+++ src/Pages/Home/Reviews/UserReviewBox.jsx
@@ -1,34 +1,44 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-undef */
 import { FaUserCheck } from "react-icons/fa";
 
-
-const UserReviewBox = () => {
-  return <div>
+const UserReviewBox = ({ review, handleDelete }) => {
+  const { feedback, image, name, designation, _id } = review;
+  return (
     <div>
-          <div className="p-6 max-w-lg mx-auto">
-            <div className="bg-white bg-opacity-5 p-6 rounded-lg shadow-md backdrop-blur-lg">
-              <p className="text-lg text-white mb-4">{feedback}</p>
-              <div className="flex items-center">
-                {image ? (
-                  <img
-                    src={image}
-                    alt={name}
-                    className="w-14 h-14 rounded-full border-2 border-white"
-                  />
-                ) : (
-                  <div className="w-14 h-14 flex items-center justify-center rounded-full bg-white p-3">
-                    <FaUserCheck className=" text-green-800 text-2xl lg:text-3xl" />
-                  </div>
-                )}
-                <div className="ml-4">
-                  <p className="text-white font-semibold text-lg">{name}</p>
-                  <p className="text-gray-300 text-sm">{designation}</p>
-                </div>
+      <div className="p-6 max-w-lg mx-auto">
+        <div className="bg-gradient-to-b from-[#249761] to-[#a1bd94] p-6 rounded-lg shadow-md backdrop-blur-lg">
+          <p className="text-lg text-black mb-4">{feedback}</p>
+          <div className="flex items-center">
+            {image ? (
+              <img
+                src={image}
+                alt={name}
+                className="w-14 h-14 rounded-full border-2 border-white"
+              />
+            ) : (
+              <div className="w-14 h-14 flex items-center justify-center rounded-full bg-white p-3">
+                <FaUserCheck className=" text-green-800 text-2xl lg:text-3xl" />
               </div>
+            )}
+            <div className="ml-4 text-black">
+              <p className=" font-semibold text-lg">{name}</p>
+              <p className=" text-sm">{designation}</p>
             </div>
           </div>
+          <div className="flex justify-end gap-3">
+            <button
+              onClick={() => handleDelete(review._id)}
+              className="btn btn-sm btn-error text-xs"
+            >
+              Delete review
+            </button>
+          </div>
         </div>
-  </div>;
+      </div>
+    </div>
+  );
 };
 
 export default UserReviewBox;
